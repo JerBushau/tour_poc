7a4eb8547f2f9531270a58c9dfdaedce0d52d605
jer bushau <exjerlock@hotmail.com>
2017-11-05 05:36:03 -0500

    improved map and info display, added README

---
+++ README.md
@@ -0,0 +1,3 @@
+An early prototype/proof of concept for a self-guided tour application.
+
+It takes JSON list of tour stops. Then uses the Google maps API to get directions to the active stop.
--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,31 @@
 'use strict'
 
-// CLEAN UP THIS MESS
-var directionsService;
-var directionsDisplay;
-var currentLocation;
-var currentDestination;
-var currentIndex = 0;
-var travelMode;
+let map;
+let directionsService;
+let directionsDisplay;
+let currentLocation;
+let currentDestination;
+let travelMode;
+let currentIndex = 0;
 
 // JSON stops
-var tourStops = [
+let tourStops = [
   {
-    name: '1) Sunergos on preston',
+    name: 'Sunergos on preston',
     location: '2122 S Preston St, Louisville, KY 40217',
-    img: 'https://static1.squarespace.com/static/54c6434de4b0badc93af5fb8/t/54e96c65e4b0a4e937ac1237/1424584349432/?format=750w',
+    img: 'https://static1.squarespace.com/static/54c6434de4b0badc93af5fb8/t/59c93036e9bfdfecd27ec3ea/1506357320082/IMG_5680.jpg?format=500w',
     info: 'Nice spot to get some work done, on the crowded side at times. Nord\'s bakery is right next door! \nArtisan sustainable kombucha butcher literally brunch. Cray PBR&B ugh church-key pug. Wolf pinterest raclette, flexitarian +1 listicle kale chips live-edge. Ugh locavore hammock blog austin tousled tumblr, food truck synth vice kale chips try-hard etsy banh mi cronut. Scenester brunch banh mi vape, fam selfies green juice tacos put a bird on it tumblr. Succulents street art single-origin coffee keffiyeh, kitsch green juice organic cornhole direct trade woke chia.'
   },
   {
-    name: '2) Sunergos on 5th',
+    name: 'Sunergos on 5th',
     location: '231 S 5th St, Louisville, KY 40202',
-    img: 'https://static1.squarespace.com/static/54c6434de4b0badc93af5fb8/t/54e96bd8e4b0feaa477006ef/1480515415116/?format=750w',
+    img: 'https://static1.squarespace.com/static/54c6434de4b0badc93af5fb8/t/59c93081f5e231573fe5e2b8/1506357455396/2017_8_Sungergos_jk_16.jpg?format=500w',
     info: 'Best coffee of the three loactions, they have a SLAYER(slayerespresso.com)! no seating area. Chicharrones vaporware everyday carry lomo, quinoa aute pop-up assumenda food truck tacos neutra artisan. Cold-pressed fam mixtape, anim fap voluptate williamsburg artisan consectetur ullamco selfies portland. Selvage edison bulb echo park accusamus chicharrones, twee butcher XOXO assumenda distillery. Proident crucifix fanny pack, pork belly nulla vexillologist qui pinterest raclette jean shorts etsy anim wolf magna austin. Est locavore microdosin. Chips four dollar toast typewriter, ut cold-pressed franzen iPhone.'
   },
   {
-    name: '3) Sunergos on Woodlawn',
+    name: 'Sunergos on Woodlawn',
     location: '306 W Woodlawn Ave, Louisville, KY 40214',
-    img: 'https://static1.squarespace.com/static/54c6434de4b0badc93af5fb8/t/54cd32f5e4b03ca9e80fc66f/1422734114254/Sunergos-4+copy.jpg?format=750w',
+    img: 'https://static1.squarespace.com/static/54c6434de4b0badc93af5fb8/t/59c9314ea9db0992707e6821/1509459546810/IMG_5785.jpg?format=500w',
     info: 'Plenty of seating, southside location. Succulents coloring book air plant, put a bird on it pok pok roof party pitchfork glossier everyday carry gentrify proident. Pour-over roof party edison bulb, consectetur ramps activated charcoal yuccie butcher bicycle rights culpa anim ullamco quinoa flannel. Tilde dolore labore magna godard hexagon single-origin coffee. Microdosing snackwave kale chips, intelligentsia plaid laborum nisi photo booth venmo pabst est id food truck hoodie pok pok.'
   }
 ];
@@ -44,12 +44,12 @@ if ("geolocation" in navigator) {
 function findCurrentLocation() {
   navigator.geolocation.watchPosition((pos) => {
     currentLocation = { lat: pos.coords.latitude, lng: pos.coords.longitude };
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
+    document.getElementById('map-btn').style.display = 'flex';
   }, (error) => {
-    // if they deny, give generic location
+    // if they deny, give them generic location
     if (error.code == error.PERMISSION_DENIED) {
       currentLocation = 'louisville, ky';
-      calculateAndDisplayRoute(directionsService, directionsDisplay);
+      document.getElementById('map-btn').style.display = 'flex';
     };
   });
 }
@@ -66,18 +66,45 @@ function setDestination() {
 // view next 'stop'
 function nextStop() {
   (currentIndex >= tourStops.length - 1) ? currentIndex = 0 : currentIndex++;
-
   setDestination();
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
+}
+
+// view prev 'stop'
+function prevStop() {
+  (currentIndex <= 0) ? currentIndex = (tourStops.length - 1) : currentIndex--;
+  setDestination();
+}
+
+// show map view
+function toggleMap() {
+  let mapWrap = document.getElementById('map-wrap');
+  let infoWrap = document.getElementById('info-wrap');
+
+  if (mapWrap.style.display === '' || mapWrap.style.display === 'none') {
+    mapWrap.style.display = 'flex';
+    infoWrap.style.display = 'none';
+    google.maps.event.trigger(map, 'resize');
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  } else {
+    mapWrap.style.display = 'none';
+    infoWrap.style.display = 'flex';
+  };
+
 }
 
 // init google map
 function initMap() {
   directionsService = new google.maps.DirectionsService;
   directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
-    scrollwheel: false
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    scrollwheel: false,
+    disableDefaultUI: true,
+    mapTypeControl: false,
+    fullscreenControl: true,
+    fullscreenControlOptions: {
+      position: google.maps.ControlPosition.LEFT_TOP
+    }
   });
 
   directionsDisplay.setMap(map);
@@ -86,7 +113,7 @@ function initMap() {
 
 // calculate and render directions
 function calculateAndDisplayRoute() {
-  var travelMode = document.getElementById('mode').value;
+  let travelMode = document.getElementById('mode').value;
 
   directionsService.route({
     origin: currentLocation,
@@ -100,4 +127,3 @@ function calculateAndDisplayRoute() {
     };
   });
 }
-
